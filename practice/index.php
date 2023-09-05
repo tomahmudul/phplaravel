f32bae8d7fe335621746d5cf9eedd9f711b9fc7f
Mahmudul Hassan <hassan.01303@gmail.com>
2023-09-05 16:54:14 +0600

    practice folder added

---
+++ practice/index.php
@@ -0,0 +1,14 @@
+<?php 
+// integer float string boolean
+$intVar = 4;
+$floatVar = 4.25; 
+$stringVar = 'Hello world';
+$boolVar = true;
+
+
+echo $intVar.'\n';
+echo $floatVar.'<br>';
+echo $stringVar.'<br>';
+echo $boolVar.'<br>';
+
+?>
