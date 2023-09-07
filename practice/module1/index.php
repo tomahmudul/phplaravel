df7d0a911ab7cd7a9df98c54754e72b52a30549c
Mahmudul Hassan <hassan.01303@gmail.com>
2023-09-07 23:07:27 +0600

    new code

--- practice/module1/index.php
+++ practice/module1/index.php
@@ -1,14 +1,21 @@
-<?php 
+<?php
 // integer float string boolean
 $intVar = 4;
-$floatVar = 4.25; 
+$floatVar = 4.25;
 $stringVar = 'Hello world';
 $boolVar = true;
 
+echo $intVar . '\n';
+echo $floatVar . '<br>';
+echo $stringVar . '<br>';
+echo $boolVar . '<br>';
 
-echo $intVar.'\n';
-echo $floatVar.'<br>';
-echo $stringVar.'<br>';
-echo $boolVar.'<br>';
+function fact( $num ) {
+    if ( $num == 0 ) {
+        return 1;
+    } else {
+        return $num * fact( $num - 1 );
+    }
+}
 
-?>
+echo fact( 5 );
