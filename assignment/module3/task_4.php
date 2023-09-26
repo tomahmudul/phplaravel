76a73d5e408de9e5376d78be415439486ce14dff
Mahmudul Hassan <hassan.01303@gmail.com>
2023-09-26 11:17:56 +0600

    assignment 3 final

--- assignment/module3/task_4.php
+++ assignment/module3/task_4.php
@@ -3,9 +3,19 @@
 
 Create a multidimensional array called $studentGrades to store the grades of three students. Each student has grades for three subjects: Math, English, and Science. Write a PHP function which takes "$studentGrades" as an argument to calculate and print the average grade for each student. */
 
-$studentGrades = [    
-    ["name"=>"Karim", "grades"=> ["Math"=> "A", "English"=>"A", "Science"=> "A" ] ],
-    ["name"=>"Rahim", "grades"=> ["Math"=> "A", "English"=>"A", "Science"=> "A" ] ],
-    ["name"=>"Hamid", "grades"=> ["Math"=> "A", "English"=>"A", "Science"=> "A" ] ]
+$studentGrades = [
+    ["name" => "Karim", "grades" => ["Math" => "100", "English" => "85", "Science" => "80"]],
+    ["name" => "Rahim", "grades" => ["Math" => "90", "English" => "90", "Science" => "80"]],
+    ["name" => "Hamid", "grades" => ["Math" => "95", "English" => "85", "Science" => "80"]],
 ];
-print_r($studentGrades);
+
+function calculateAverageGrades( $student ) {
+    foreach ( $student as $key => $value ) {
+        $average = array_sum( $value["grades"] ) / count( $value["grades"] );
+        $student[$key]['averageGrade'] = number_format( $average, 2 );
+    }
+    return $student;
+}
+
+$averageGrade = calculateAverageGrades( $studentGrades );
+print_r( $averageGrade );
