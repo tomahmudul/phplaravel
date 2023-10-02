b2db7953643e74f5d874852f39bff20df83d2e3a
Mahmudul Hassan <hassan.01303@gmail.com>
2023-10-02 23:56:41 +0600

    module 4

--- understand_php/module4/coding_test.php
+++ understand_php/module4/coding_test.php
@@ -1,21 +1,42 @@
 <?php
 //remove duplicate char
-$str = "1AAABBCCCDDDDDDD";
-echo $result = count_chars( $str, 3);
-
-$str = 'foo,bar,foo,baz';
-$uniqueStr = implode(',', array_keys(array_flip(explode(',', $str))));
-echo $uniqueStr;
+$str = "apples";
+// fscanf(STDIN, "%s", $str);     
+$chars = str_split( $str );
+echo $result = implode("",array_unique($chars , SORT_STRING)); 
 
 //check anagram
-function is_anagram($string_1, $string_2)
-{
-    if (count_chars($string_1, 1) == count_chars($string_2, 1))
-        return 'yes';
-    else
-        return 'no';	
-}
 
-// Driver code
-print_r(is_anagram('stop', 'post')."");
-print_r(is_anagram('card', 'cart')."");
+# Write your PHP code from here
+// $str1 = fgets(STDIN);
+// $str2 = fgets(STDIN);
+
+$str1 = "abcd";  
+$str2 = "aabc";  
+    
+// $str1 = fgets(STDIN);
+// $str2 = fgets(STDIN);
+
+
+if(strlen($str1) != strlen($str2)){  
+echo "NO";  
+}else{     
+$str1 = strtolower($str1);  
+$str2 = strtolower($str2);    
+
+$str1array = str_split($str1);  
+$str2array = str_split($str2);  
+
+sort($str1array);  
+sort($str2array);      
+
+$str1 = implode("",$str1array);  
+$str2 = implode("",$str2array);
+
+if(strcmp($str1,$str2) == 0){  
+    echo "YES";  
+}  
+else{  
+    echo "NO";  
+}  
+}  
